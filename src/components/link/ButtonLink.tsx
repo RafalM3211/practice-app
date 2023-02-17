9b6450a416a70367d62f46bd7e83ef871c284fb9
Rafal Malinowski <cyrax1321@gmail.com>
2023-02-17 21:37:30 +0100

    change buttonLink to typescript and add tsconfig.json

--- src/components/link/ButtonLink.tsx
+++ src/components/link/ButtonLink.tsx
@@ -1,7 +1,14 @@
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { ReactNode } from 'react';
 
-const ButtonLink = ({ children, to: path, variant, ...props }) => {
+type Props={
+  children: ReactNode,
+  to: string,
+  variant: 'contained' | 'outlined'| 'text'
+}
+
+const ButtonLink = ({ children, to: path, variant, ...props }: Props) => {
   return (
     <Link style={{ textDecoration: 'none' }} to={path}>
       <Button {...props} variant={variant}>
