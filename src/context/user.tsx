22e9c5eb767b1486b0204ca57c0a0b623d0ec4cd
Rafal Malinowski <cyrax1321@gmail.com>
2023-03-08 13:35:09 +0100

    moved user provider to typescript

--- src/context/user.tsx
+++ src/context/user.tsx
@@ -2,13 +2,35 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Login from '../views/Login/Login';
 import LoaderOverlay from '../components/loaderOverlay/LoaderOverlay';
 import { useContext } from 'react';
+import type { ReactNode, FC } from 'react'
 
-const UserContext = React.createContext(undefined);
+const UserContext = React.createContext<UserContextType | undefined>(undefined);
 export const useUserContext = () => {
-  return useContext(UserContext);
+  const contextValue=useContext(UserContext);
+  if(!contextValue){
+    throw new Error("Dont use user context outside provider");
+  }
+  return contextValue;
 };
 
-const UserProvider = ({ children }) => {
+interface UserType {
+  isLoggedIn: boolean,
+  name: string,
+  logOut(): void
+}
+
+interface UserContextType {
+  user: UserType,
+  updateUser(newData: Partial<UserType>): void
+}
+
+interface Props {
+  children: ReactNode
+}
+
+type Token = string | null
+
+const UserProvider: FC<Props> = ({ children }) => {
   const initialUserState = {
     isLoggedIn: false,
     name: '',
@@ -20,12 +42,12 @@ const UserProvider = ({ children }) => {
   const [isTokenLoading, setIsTokenLoading] = useState(true);
   const [user, setUser] = useState(initialUserState);
   
-  const updateUser = useCallback((newData) => {
+  const updateUser = useCallback((newData: Partial<UserType>) => {
     setUser(currentUser=>({ ...currentUser, ...newData }));
   }, [setUser]);
 
   useEffect(() => {
-    const checkToken = async (token) => {
+    const checkToken = async (token: Token) => {
       console.log('token', token);
       return !!token;
     };
@@ -34,7 +56,7 @@ const UserProvider = ({ children }) => {
     const userName = sessionStorage.getItem('userName');
     checkToken(token).then((isValid) => {
       setIsTokenLoading(false);
-      if (isValid) updateUser({ name: userName, isLoggedIn: true });
+      if (isValid) updateUser({ name: userName as string, isLoggedIn: true });
       else updateUser({ name: '', isLoggedIn: false });
     });
   }, [updateUser]);
