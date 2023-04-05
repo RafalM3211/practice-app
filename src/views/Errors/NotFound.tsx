9c903abe7e6bd11114b5fc75eda1d59f2fdfa7d7
Rafal Malinowski <cyrax1321@gmail.com>
2023-04-05 22:47:20 +0200

    not found error view moved to typescript

--- src/views/Errors/NotFound.tsx
+++ src/views/Errors/NotFound.tsx
@@ -2,14 +2,16 @@ import { Box } from '@mui/material';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import ButtonLink from '../../components/link/ButtonLink';
+import type { Theme } from '@mui/material/styles';
 
-const calcHeightWithBreakpoints = (theme) => {
-  const smToolbarHeight = theme.mixins.toolbar['@media (min-width:600px)'].minHeight;
+const calcHeightWithBreakpoints = (theme: Theme) => {
+  const smToolbarHeight = theme.mixins.toolbar['@media (min-width:600px)'] as number;
   const xsToolbarHeight = theme.mixins.toolbar.minHeight;
+  if(xsToolbarHeight==undefined) throw new Error("min height set to undefinded");
   return { xs: calcForToolbarHeight(xsToolbarHeight), sm: calcForToolbarHeight(smToolbarHeight) };
 };
 
-const calcForToolbarHeight = (toolbarHeight) => {
+const calcForToolbarHeight = (toolbarHeight: number|string) => {
   return `calc(100vh - ${toolbarHeight}px)`;
 };
 
@@ -29,7 +31,7 @@ const NotFound = () => {
         <Typography variant="h2" component="h2">
           Błąd 404
         </Typography>
-        <Typography sx={{ fontSize: '1.4em' }} variant="body" component="p">
+        <Typography sx={{ fontSize: '1.4em' }} variant="body1" component="p">
           podana strona nie istnieje
         </Typography>
         <ButtonLink
