df5eff353f115e336a9b8af3c9f46a75455c9747
Rafal Malinowski <cyrax1321@gmail.com>
2023-04-05 22:48:28 +0200

    improve button link props types to allow any number of optional props

--- src/components/link/ButtonLink.tsx
+++ src/components/link/ButtonLink.tsx
@@ -2,11 +2,14 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import { ReactNode } from 'react';
 import type { FC } from 'react';
+import type { SxProps, Theme } from '@mui/material/styles';
 
 interface Props{
   children: ReactNode,
   to: string,
-  variant: 'contained' | 'outlined'| 'text'
+  variant: 'contained' | 'outlined'| 'text',
+  sx?: SxProps<Theme>,
+  [rest: string]: any
 }
 
 const ButtonLink: FC<Props> = ({ children, to: path, variant, ...props }) => {
